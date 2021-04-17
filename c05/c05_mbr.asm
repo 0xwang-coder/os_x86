658246a699d37d7f0d6c8b133546c092f84b4ed5
wwangwanchao@126.com <wwangwanchao@126.com>
2021-04-17 12:51:56 +0800

    first

--- c05/c05_mbr.asm
+++ c05/c05_mbr.asm
@@ -1,103 +1,104 @@
-         ;代码清单5-1 
-         ;文件名：c05_mbr.asm
-         ;文件说明：硬盘主引导扇区代码
-         ;创建日期：2011-3-31 21:15 
-         
-         mov ax,0xb800                 ;指向文本模式的显示缓冲区
-         mov es,ax
+;5-1 
+;05_mbr.asm
+;
+;31 21:15 
 
-         ;以下显示字符串"Label offset:"
-         mov byte [es:0x00],'L'
-         mov byte [es:0x01],0x07
-         mov byte [es:0x02],'a'
-         mov byte [es:0x03],0x07
-         mov byte [es:0x04],'b'
-         mov byte [es:0x05],0x07
-         mov byte [es:0x06],'e'
-         mov byte [es:0x07],0x07
-         mov byte [es:0x08],'l'
-         mov byte [es:0x09],0x07
-         mov byte [es:0x0a],' '
-         mov byte [es:0x0b],0x07
-         mov byte [es:0x0c],"o"
-         mov byte [es:0x0d],0x07
-         mov byte [es:0x0e],'f'
-         mov byte [es:0x0f],0x07
-         mov byte [es:0x10],'f'
-         mov byte [es:0x11],0x07
-         mov byte [es:0x12],'s'
-         mov byte [es:0x13],0x07
-         mov byte [es:0x14],'e'
-         mov byte [es:0x15],0x07
-         mov byte [es:0x16],'t'
-         mov byte [es:0x17],0x07
-         mov byte [es:0x18],':'
-         mov byte [es:0x19],0x07
+mov ax,0xb800                 ;指
+mov es,ax
 
-         mov ax,number                 ;取得标号number的偏移地址
-         mov bx,10
+; show ASCII code "Label offset:"
+mov byte [es:0x00],'L'
+mov byte [es:0x01],0x07
+mov byte [es:0x02],'a'
+mov byte [es:0x03],0x07
+mov byte [es:0x04],'b'
+mov byte [es:0x05],0x07
+mov byte [es:0x06],'e'
+mov byte [es:0x07],0x07
+mov byte [es:0x08],'l'
+mov byte [es:0x09],0x07
+mov byte [es:0x0a],' '
+mov byte [es:0x0b],0x07
+mov byte [es:0x0c],"o"
+mov byte [es:0x0d],0x07
+mov byte [es:0x0e],'f'
+mov byte [es:0x0f],0x07
+mov byte [es:0x10],'f'
+mov byte [es:0x11],0x07
+mov byte [es:0x12],'s'
+mov byte [es:0x13],0x07
+mov byte [es:0x14],'e'
+mov byte [es:0x15],0x07
+mov byte [es:0x16],'t'
+mov byte [es:0x17],0x07
+mov byte [es:0x18],':'
+mov byte [es:0x19],0x07
 
-         ;设置数据段的基地址
-         mov cx,cs
-         mov ds,cx
+mov ax,number                 ; set number offset
+mov bx,10
 
-         ;求个位上的数字
-         mov dx,0
-         div bx
-         mov [0x7c00+number+0x00],dl   ;保存个位上的数字
+; set data seg base addr
+mov cx,cs
+mov ds,cx
 
-         ;求十位上的数字
-         xor dx,dx
-         div bx
-         mov [0x7c00+number+0x01],dl   ;保存十位上的数字
+;
+mov dx,0
+div bx
+mov [0x7c00+number+0x00],dl   ;
 
-         ;求百位上的数字
-         xor dx,dx
-         div bx
-         mov [0x7c00+number+0x02],dl   ;保存百位上的数字
+;
+xor dx,dx
+div bx
+mov [0x7c00+number+0x01],dl   ;
 
-         ;求千位上的数字
-         xor dx,dx
-         div bx
-         mov [0x7c00+number+0x03],dl   ;保存千位上的数字
+;
+xor dx,dx
+div bx
+mov [0x7c00+number+0x02],dl   ;
 
-         ;求万位上的数字 
-         xor dx,dx
-         div bx
-         mov [0x7c00+number+0x04],dl   ;保存万位上的数字
+;
+xor dx,dx
+div bx
+mov [0x7c00+number+0x03],dl   ;
 
-         ;以下用十进制显示标号的偏移地址
-         mov al,[0x7c00+number+0x04]
-         add al,0x30
-         mov [es:0x1a],al
-         mov byte [es:0x1b],0x04
-         
-         mov al,[0x7c00+number+0x03]
-         add al,0x30
-         mov [es:0x1c],al
-         mov byte [es:0x1d],0x04
-         
-         mov al,[0x7c00+number+0x02]
-         add al,0x30
-         mov [es:0x1e],al
-         mov byte [es:0x1f],0x04
+;
+xor dx,dx
+div bx
+mov [0x7c00+number+0x04],dl   ;
 
-         mov al,[0x7c00+number+0x01]
-         add al,0x30
-         mov [es:0x20],al
-         mov byte [es:0x21],0x04
+; show offset
+mov al,[0x7c00+number+0x04]
+add al,0x30
+mov [es:0x1a],al
+mov byte [es:0x1b],0x04
 
-         mov al,[0x7c00+number+0x00]
-         add al,0x30
-         mov [es:0x22],al
-         mov byte [es:0x23],0x04
-         
-         mov byte [es:0x24],'D'
-         mov byte [es:0x25],0x07
-          
-   infi: jmp near infi                 ;无限循环
-      
-  number db 0,0,0,0,0
-  
-  times 203 db 0
-            db 0x55,0xaa
+mov al,[0x7c00+number+0x03]
+add al,0x30
+mov [es:0x1c],al
+mov byte [es:0x1d],0x04
+
+mov al,[0x7c00+number+0x02]
+add al,0x30
+mov [es:0x1e],al
+mov byte [es:0x1f],0x04
+
+mov al,[0x7c00+number+0x01]
+add al,0x30
+mov [es:0x20],al
+mov byte [es:0x21],0x04
+
+mov al,[0x7c00+number+0x00]
+add al,0x30
+mov [es:0x22],al
+mov byte [es:0x23],0x04
+
+; ASCII
+mov byte [es:0x24],'D'
+mov byte [es:0x25],0x07
+
+infi: jmp near infi                 ; loop
+
+number db 0,0,0,0,0
+
+times 203 db 0
+      db 0x55,0xaa
